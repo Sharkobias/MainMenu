0ee0664973708cabc4aae192927f58899c5c1912
TF <toby.fajks@gmail.com>
2024-03-17 16:31:42 +0100

    added random number function and program exit:3

--- Main_Menu.cpp
+++ Main_Menu.cpp
@@ -5,7 +5,6 @@
 #include <thread>
 #include <chrono>
 using namespace std;
-
 int MenuStart()
 {
 	system("CLS");
@@ -16,6 +15,10 @@ int MenuStart()
 	cout << "|" << "   [1]CALCULATOR      " << "|" << endl;
 	cout << "|" << "     [2]CINEMA        " << "|" << endl;
 	cout << "|" << "      [3]MEAN         " << "|" << endl;
+	cout << "|" << "     [4]RANDOM        " << "|" << endl;
+	cout << " " << endl;
+	cout << "|" << "  enter [0] to exit   " << "|" << endl;
+	cout << "|" << "     or go back       " << "|" << endl;
 	cout << " " << "______________________" << " " << endl;
 	cout << " " << endl;
 	int choice;
@@ -25,17 +28,22 @@ int MenuStart()
 class Calculator
 {
 public:
-	void GetCalculation() 
+	bool GetCalculation()
 	{
+		string str;
 		string problem;
 		double result = 0;
 		cout << "write your mathematical problem with space delinated:" << endl;
 		getline(cin, problem);
+		if (problem == "0")
+		{
+			return false;
+		}
 		stringstream ss(problem);
 		vector<string> split_problem;
 		string part;
 
-		while (getline(ss,part, ' '))
+		while (getline(ss, part, ' '))
 		{
 			split_problem.push_back(part);
 		}
@@ -51,22 +59,27 @@ public:
 		{
 			result = stod(split_problem[0]) * stod(split_problem[2]);
 		}
-		else if (split_problem[1] == "/" )
+		else if (split_problem[1] == "/")
 		{
 			result = stod(split_problem[0]) / stod(split_problem[2]);
 		}
-		else 
+		else
 		{
 			cout << "unsupported type of equation" << endl;
 		}
 		cout << "equals: " << result << endl;
+		return true;
 
 	}
-	void GetMean()
+	bool GetMean()
 	{
 		string input;
 		cout << "enter your numbers space delinated" << endl;
 		getline(cin, input);
+		if (input == "0")
+		{
+			return false;
+		}
 		string num;
 		stringstream ss(input);
 		vector<double> numbers;
@@ -80,20 +93,38 @@ public:
 		{
 			sum += i;
 		}
-		cout << "Your mean equals:" << " " << sum / numbers.size()<< endl;
+		cout << "Your mean equals:" << " " << sum / numbers.size() << endl;
+		return true;
+	}
+	bool GetRandom()
+	{
+		int max;
+		int min;
+		cout << "Input smaller number in range" << endl;
+		cin >> min;
+		cout << "Input bigger number in range" << endl;
+		cin >> max;
+		if (min, max == 0)
+		{
+			return false;
+		}
+		cout << rand() % min + max << endl;
+		return true;
 	}
 };
 void PlayAnim()
 {
+	int t = 10;
 	int i = 0;
-	char spinnin[4] = {'|','/','--','\\'};
-	while (true)
+	char spinnin[4] = { '|','/','-','\\' };
+	while (t > 0)
 	{
-		system("CLs");
+		system("CLS");
 		cout << spinnin[i] << endl;
 		this_thread::sleep_for(chrono::milliseconds(300));
 		i += 1;
 		i = i % 4;
+		t = t - 1;
 	}
 }
 
@@ -106,23 +137,33 @@ int main()
 	{
 		cout << "You entered Calculator" << endl;
 		cin.ignore();
-		while (true)
-		{
-			calculate.GetCalculation();
-		}
+		while (calculate.GetCalculation()) {}
 	}
-	else if (choice == 2) 
+	else if (choice == 2)
 	{
 		PlayAnim();
 	}
-	else if (choice == 3) 
+	else if (choice == 3)
 	{
 		cout << "You entered Mean Calculator" << endl;
 		cin.ignore();
-		while (true)
-		{
-			calculate.GetMean();
-		}
+		while (calculate.GetMean()) {}
+	}
+	else if (choice == 4)
+	{
+		cout << "You have entered random number generator" << endl;
+		cin.ignore();
+		while (calculate.GetRandom()) {}
+	}
+	else if (choice == 0)
+	{
+		cout << "you exited the program" << endl;
+		abort;
+	}
+	else
+	{
+		system("CLS");
+		cout << "No function found underneath that symbol, try again" << endl;
 	}
 	return 0;
 }
